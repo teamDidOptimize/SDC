c63ef24faa83f6e110b480c1f95676a14cef438a
Joey Rivera <josephrivera5000@gmail.com>
2021-06-23 09:29:05 -0700

    init

---
+++ server.js
@@ -0,0 +1,23 @@
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').load();
+}
+
+// setup express server
+const express = require('express');
+const app = express();
+const indexRouter = require('./routes/index');
+
+app.use(express.static('public'));
+
+// setup database connections
+const mongoose = require('mongoose');
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
+const db = mongoose.connection;
+db.on('error', error => console.error(error));
+db.once('open', () => console.log('Connected to Mongoose'));
+
+// set initial routes
+app.use('/', indexRouter);
+
+// setup port listening
+app.listen(process.env.PORT || 3000);
